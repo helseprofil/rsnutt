403791278bbb61a140bf9a62dd17921cb166b543
Vegard Lysne <vegard.lysne@fhi.no>
2024-01-25 08:24:40 +0100

    include censoring of bydel

--- kube/Rsynt_Postprosess_MOBBING_1aar_0.R
+++ kube/Rsynt_Postprosess_MOBBING_1aar_0.R
@@ -1,12 +1,14 @@
 # title: "Rsynt_Postprosess_MOBBING_1aar_0"
 # author: "Vegard"
-# updated: "2024-01-24"
+# updated: "2024-01-25"
 # 
 # Retrieve data from UDIR via API
-# Identifies all strata which is censored by UDIR to make sure we do not show these data. 
+# Identifies all kommunestrata which is censored by UDIR to make sure we do not show these data. 
+# Identifies bydelstrata with only one censored school, where we cannot show data on bydel
 
 library(httr2)
 library(data.table)
+library(norgeo)
 
 cat("\n\nSTARTER RSYNT_POSTPROSESS, R-SNUTT\n")
 cat("\nHenter prikkeinformasjon fra UDIR, f.o.m. 2021-2022")
@@ -48,18 +50,36 @@ for(i in 1:pages){
                               newpage))
 }
 
-# Data wrangling
-udirprikk <- udirprikk[EnhetNivaa == 3 & AndelMobbet == "*"]
-udirprikk[, `:=` (GEO = Kommunekode,
-                  KJONN = 0,
-                  TRINN = TrinnKode,
-                  UDIRPRIKK = 1,
-                  AAR = paste0(substr(Skoleaarnavn, 1,4), "_", 
-                               as.integer(substr(Skoleaarnavn, 1,4))+1))]
-udirprikk <- udirprikk[, .(GEO, AAR, KJONN, TRINN, UDIRPRIKK)]
+# Identify censored strata kommune
+udirprikk_kommune <- udirprikk[EnhetNivaa == 3 & AndelMobbet == "*"]
+udirprikk_kommune[, `:=` (GEO = Kommunekode,
+                          KJONN = 0,
+                          TRINN = TrinnKode,
+                          UDIRPRIKK = 1,
+                          AAR = paste0(substr(Skoleaarnavn, 1,4), "_", 
+                                       as.integer(substr(Skoleaarnavn, 1,4))+1))]
+udirprikk_kommune <- udirprikk_kommune[, .(GEO, AAR, KJONN, TRINN, UDIRPRIKK)]
+
+# Identify bydelstrata with one censored school
+udirprikk_bydel <- udirprikk[EnhetNivaa == 4, .(Skoleaarnavn, Organisasjonsnummer, EnhetNavn, AndelMobbet, TrinnKode)]
+skolebydel <- fread("https://raw.githubusercontent.com/helseprofil/snutter/main/misc/SkoleBydel.csv", 
+                    colClasses=list(character=c("OrgNo","GEO")))
+udirprikk_bydel <- udirprikk_bydel[skolebydel, `:=` (GEO = i.GEO), on = c(Organisasjonsnummer = "OrgNo")][!is.na(GEO)]
+udirprikk_bydel[, `:=` (KJONN = 0,
+                        TRINN = TrinnKode,
+                        AAR = paste0(substr(Skoleaarnavn, 1,4), "_", 
+                                     as.integer(substr(Skoleaarnavn, 1,4))+1))]
+
+udirprikk_bydel <- udirprikk_bydel[, .(UDIRPRIKK = sum(AndelMobbet == "*", na.rm = T)), 
+                   by = c("GEO", "AAR", "KJONN", "TRINN")][UDIRPRIKK == 1]
+
+
+# Combine lists of strata to censor
+censor <- data.table::rbindlist(list(udirprikk_kommune,
+                                     udirprikk_bydel))
 
 # Merge udirdata
-KUBE <- merge.data.table(KUBE, udirprikk, by = c("GEO", "AAR", "KJONN", "TRINN"), all.x = T)
+KUBE <- merge.data.table(KUBE, censor, by = c("GEO", "AAR", "KJONN", "TRINN"), all.x = T)
 
 # Save object UDIRPRIKKpre
 UDIRPRIKKpre <<- KUBE[UDIRPRIKK == 1]
