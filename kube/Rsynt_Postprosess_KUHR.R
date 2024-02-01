469754a69ea9dd1ff8693cf63135b4694205540d
Vegard Lysne <vegard.lysne@fhi.no>
2024-02-01 10:54:20 +0100

    delete data on Aalesund/Haram for 2020-2023

--- kube/Rsynt_Postprosess_KUHR.R
+++ kube/Rsynt_Postprosess_KUHR.R
@@ -1,8 +1,8 @@
 # RSYNT_POSTPROSESS for kube KUHR
-# Skrevet av: VL 11.jan 2024
+# Author: Vegard Lysne
+# Updated: 2024.02.01
 
-# Sletter tall på bydelsnivå for 2023
-# Fordi: For høy andel uoppgitt bydel
+# Delete bydels for 2023, due to too much unknown
 
 KUBE[nchar(GEO) > 4 & 
        grepl("^1103", GEO) & 
@@ -20,3 +20,14 @@ KUBE[AAR == "2020_2022" &
        MEIS = NA_real_,
        TELLER.f = 1,
        RATE.f = 1)]
+
+# Delete all numbers for Aalesund (1508) and Haram (1580) for periods including years 2020-2023.
+# In this period, these data are provided using the common GEO-code 1507 valid before splitting up.
+
+KUBE[GEO %in% c("1508", "1580") & !(AARl < 2020 & AARh < 2020 |AARl > 2023 & AARh > 2023),
+     `:=`
+     (RATE = NA_real_,
+     SMR = NA_real_,
+     MEIS = NA_real_,
+     TELLER.f = 1,
+     RATE.f = 1)]
